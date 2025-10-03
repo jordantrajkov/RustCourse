2cf96c5f5ca1d98acc6544692af19e1a03da4e6d
Jordan Trajkov <jordan.trajkov@novelic.com>
2025-10-03 09:58:05 +0200

    Initial commit: Hello World program

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Joce");
+    println!("Rust");
+    println!("I'm learning Rust to build fast, reliable, and memory-safe software while improving systems programming skills.");
+}
