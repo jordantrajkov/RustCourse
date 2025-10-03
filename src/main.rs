38ae46dd04b143b83e30db83b5b333e9a8c439af
Jordan Trajkov <jordan.trajkov@novelic.com>
2025-10-03 10:07:07 +0200

    Changed main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,8 @@ fn main() {
     println!("Joce");
     println!("Rust");
     println!("I'm learning Rust to build fast, reliable, and memory-safe software while improving systems programming skills.");
+    println!("Make a difference with Rust!");
 }
+
+
+
